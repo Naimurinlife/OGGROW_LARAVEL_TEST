32bee49b5ab686fb87d01aaf4c8efbe495661c86
Naimur <naimurshuvo.bd.16@gmail.com>
2023-07-17 03:23:22 +0600

    API Creation Error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,25 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+//use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
+
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
+
+    public function register():void
     {
-        //
+        Passport::ignoreRoutes();
+
+
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
+
+        Passport::routes();
     }
 }
