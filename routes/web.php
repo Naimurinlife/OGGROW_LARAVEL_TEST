2adaffd675d86553a29c9c336668c5f6d7f39405
Naimur <naimurshuvo.bd.16@gmail.com>
2023-07-17 13:09:34 +0600

    Passport Route Added

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,33 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use Laravel\Passport\Http\Controllers\AccessTokenController;
+use Laravel\Passport\Http\Controllers\AuthorizationController;
+use Laravel\Passport\Http\Controllers\ClientController;
+use Laravel\Passport\Http\Controllers\PersonalAccessTokenController;
+
+
+// Authorization Endpoint
+Route::get('/oauth/authorize', [AuthorizationController::class, 'authorize'])
+    ->middleware(['web', 'auth']);
+
+Route::post('/oauth/authorize', [AuthorizationController::class, 'approve'])
+    ->middleware(['web', 'auth']);
+
+Route::delete('/oauth/authorize', [AuthorizationController::class, 'deny'])
+    ->middleware(['web', 'auth']);
+
+// Access Token Endpoint
+Route::post('/oauth/token', [AccessTokenController::class, 'issueToken'])
+    ->middleware(['throttle', 'web', 'auth']);
+
+// Client Routes
+Route::resource('/oauth/clients', ClientController::class)
+    ->middleware(['web', 'auth']);
+
+// Personal Access Token Routes
+Route::resource('/oauth/personal-access-tokens', PersonalAccessTokenController::class)
+    ->middleware(['web', 'auth']);
 
 Route::get('/', function () {
     return view('welcome');
